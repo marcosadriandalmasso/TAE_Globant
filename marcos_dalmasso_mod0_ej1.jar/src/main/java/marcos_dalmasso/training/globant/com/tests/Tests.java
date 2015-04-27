60cf2f29b8857f7fcf2af2823aada0f1e63154a1
marcosadriandalmasso <marcosadriandalmasso@hotmail.com>
2015-04-27 12:56:49 -0300

    Se descomentaron los tests de la clase Tests

--- marcos_dalmasso_mod0_ej1.jar/src/main/java/marcos_dalmasso/training/globant/com/tests/Tests.java
+++ marcos_dalmasso_mod0_ej1.jar/src/main/java/marcos_dalmasso/training/globant/com/tests/Tests.java
@@ -1,6 +1,5 @@
 package marcos_dalmasso.training.globant.com.tests;
 
-//import junit.framework.Assert;
 import marcos_dalmasso.training.globant.com.pages.ContactUsPage;
 import marcos_dalmasso.training.globant.com.pages.HomePage;
 import marcos_dalmasso.training.globant.com.pages.PostPage;
@@ -9,7 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -26,53 +24,53 @@ public class Tests {
 		driver = new FirefoxDriver();
 	}
 	
-//	@AfterMethod
-//	public void after() {
-//		driver.quit();
-//	}
-//	
-//	@Test
-//	public void validatePageByTitleTest() {
-//		HomePage homePage = PageFactory.initElements(driver, HomePage.class);
-//		homePage.go(driver);
-//		Assert.assertEquals(driver.getTitle(), "Automation Training | Aprender a automatizar en un solo sitio");
-//	}
-//	
-//	@Test
-//	public void searchQueryTest() {
-//		HomePage homePage = PageFactory.initElements(driver, HomePage.class);
-//		homePage.go(driver);
-//		homePage.searchQuery("");
-//		Assert.assertEquals(homePage.getSearchResult().contains("Nothing Found"), true, "There are search results");
-//	}
-//	
-//	@Test
-//	public void validatePostsDateTest() {
-//		HomePage homePage = PageFactory.initElements(driver, HomePage.class);
-//		homePage.go(driver);
-//		PostPage postPage = PageFactory.initElements(driver, PostPage.class);
-//		postPage.getInFirstPost();
-//		Assert.assertEquals(postPage.getPostsDate(), homePage.getPostDate(), "The date is incorrect");
-//	}
-//	
-//	@Test
-//	public void validateContactFormTest() {
-//		ContactUsPage contactUsPage = PageFactory.initElements(driver, ContactUsPage.class);
-//		contactUsPage.go(driver);
-//		contactUsPage.fillInContactForm("Marcos", "tae@globan.com", "Automation Testing", "Testing");
-//		Assert.assertEquals(contactUsPage.getContactConfirmationMsj(), "Thank you for contacting us.", "Error submitting the form.");
-//	}
-//	
-//	@Test
-//	public void validateContactFormTest2() {
-//		ContactUsPage contactUsPage = PageFactory.initElements(driver, ContactUsPage.class);
-//		contactUsPage.go(driver);
-//		contactUsPage.fillIncompleteContactForm("Marcos", "Automation Testing", "Testing");
-//		if(contactUsPage.getContactErrorMsj().equalsIgnoreCase("Please make corrections below and try again.")) {
-//			contactUsPage.fillIncompleteContactForm2("tae@globant.com");
-//			Assert.assertEquals(contactUsPage.getContactConfirmationMsj(), "Thank you for contacting us.", "Error submitting the form.");
-//		}
-//	}
+	@AfterMethod
+	public void after() {
+		driver.quit();
+	}
+	
+	@Test
+	public void validatePageByTitleTest() {
+		HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+		homePage.go(driver);
+		Assert.assertEquals(driver.getTitle(), "Automation Training | Aprender a automatizar en un solo sitio");
+	}
+	
+	@Test
+	public void searchQueryTest() {
+		HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+		homePage.go(driver);
+		homePage.searchQuery("");
+		Assert.assertEquals(homePage.getSearchResult().contains("Nothing Found"), true, "There are search results");
+	}
+	
+	@Test
+	public void validatePostsDateTest() {
+		HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+		homePage.go(driver);
+		PostPage postPage = PageFactory.initElements(driver, PostPage.class);
+		postPage.getInFirstPost();
+		Assert.assertEquals(postPage.getPostsDate(), homePage.getPostDate(), "The date is incorrect");
+	}
+	
+	@Test
+	public void validateContactFormTest() {
+		ContactUsPage contactUsPage = PageFactory.initElements(driver, ContactUsPage.class);
+		contactUsPage.go(driver);
+		contactUsPage.fillInContactForm("Marcos", "tae@globan.com", "Automation Testing", "Testing");
+		Assert.assertEquals(contactUsPage.getContactConfirmationMsj(), "Thank you for contacting us.", "Error submitting the form.");
+	}
+	
+	@Test
+	public void validateContactFormTest2() {
+		ContactUsPage contactUsPage = PageFactory.initElements(driver, ContactUsPage.class);
+		contactUsPage.go(driver);
+		contactUsPage.fillIncompleteContactForm("Marcos", "Automation Testing", "Testing");
+		if(contactUsPage.getContactErrorMsj().equalsIgnoreCase("Please make corrections below and try again.")) {
+			contactUsPage.fillIncompleteContactForm2("tae@globant.com");
+			Assert.assertEquals(contactUsPage.getContactConfirmationMsj(), "Thank you for contacting us.", "Error submitting the form.");
+		}
+	}
 	
 	@Test
 	public void postsByCalendarTest() {
@@ -84,5 +82,4 @@ public class Tests {
 		homePage.findMonthlyPosts();
 		homePage.countPosts();
 	}
-	
 }
